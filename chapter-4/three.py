34a2863aff8540359b12d0125369fc9da2207955
Irene06he <104945913+Irene06he@users.noreply.github.com>
2022-09-08 09:58:58 +0000

    I modified three.py

---
+++ chapter-4/three.py
@@ -0,0 +1,4 @@
+# exercise 3
+
+for month in ["January", "February", "March", "April", "May", "June", "July","August","September","October","November","December"]:
+    print("One of the months of the years is", month)
