5e495c50ff05323d2fa1af29eff27eec72641ba9
Irene06he <104945913+Irene06he@users.noreply.github.com>
2022-09-08 10:32:03 +0000

    I modified seven.py

---
+++ chapter-4/seven.py
@@ -0,0 +1,28 @@
+#exercise 7
+
+import turtle              
+wn = turtle.Screen() 
+
+crush = turtle.Turtle()      
+crush.forward(100)           
+crush.left(160)               
+crush.forward(100)
+crush.left(-43)
+crush.forward(100)
+crush.left(270)
+crush.forward(100)
+crush.left(-97)
+crush.forward(100)
+crush.left(-43)
+crush.forward(100)
+crush.left(200)
+crush.forward(100)
+crush.left(-940)
+crush.forward(100)
+crush.left(17)
+crush.forward(100)
+crush.left(-86)
+
+print("The pirate's current heading is", crush.heading())
+
+wn.exitonclick()
