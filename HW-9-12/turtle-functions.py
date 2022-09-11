e0612f0531c34224ca1ab74b71ab3ecc0d43fbfc
Irene06he <104945913+Irene06he@users.noreply.github.com>
2022-09-11 07:16:02 +0000

    I modified file turtle-functions.py

--- HW-9-12/turtle-functions.py
+++ HW-9-12/turtle-functions.py
@@ -14,17 +14,18 @@ def hexagon(t,x,y,color,width,sidelen):
         t.forward (sidelen)
         t.right(60)
  
-def ngon(t, numsides,x,y,color,width,sidelen):
+def ngon(t,numsides,x,y,color,width,sidelen):
     # code to draw a ngon
     t.penup()
     t.goto(x,y)
     t.color(color)
     t.width(width)
     t.pendown()
+    myangle = 360/numsides
     # draw a ngon
     for i in range (numsides):
         t.forward(sidelen)
-        t.right(40)
+        t.right(myangle)
     
 wn = turtle.Screen()
 
@@ -32,7 +33,7 @@ crush = turtle. Turtle()
 hexagon(crush,40,40,"blue",6,50)
 
 squirt = turtle. Turtle()
-ngon(squirt,9,100,100,"pink",5,50)
+ngon(squirt,12,100,100,"pink",5,50)
 
 
 wn.exitonclick()
