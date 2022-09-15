56adf41165a64b1eef052e267bbef76356ace18c
Irene06he <104945913+Irene06he@users.noreply.github.com>
2022-09-15 13:16:23 +0000

    I modified file answer.py

--- 9-15/answer.py
+++ 9-15/answer.py
@@ -27,21 +27,15 @@ print(isRightAngle2(5,3,4))
 
 
 # exercise 7.11 #
-from test import testEqual
+def isRightAngle(a,b,c):
+    return a*a + b*b == c*c
 
-def is_rightangled(a, b, c):
-    is_rightangled = False
+def isRightAngle2(a,b,c):
+    return isRightAngle(a,b,c) or \
+            isRightAngle(b,c,a) or \
+            isRightAngle(a,c,b)
 
-    if a > b and a > c:
-        is_rightangled = abs(b**2 + c**2 - a**2) < 0.001
-    elif b > a and b > c:
-        is_rightangled = abs(a**2 + c**2 - b**2) < 0.001
-    else:
-        is_rightangled = abs(a**2 + b**2 - c**2) < 0.001
-    return is_rightangled
-
-testEqual(is_rightangled(1.5, 2.0, 2.5), True)
-testEqual(is_rightangled(4.0, 8.0, 16.0), False)
+print(isRightAngle2(5,3,4))
 
 
 # hello_name #
