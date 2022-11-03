16f0d7efc89d5a74c575394d59d07f6d8ef0b409
Irene06he <104945913+Irene06he@users.noreply.github.com>
2022-11-03 12:30:38 +0000

    I modified modes.py

--- mode/modes.py
+++ mode/modes.py
@@ -12,3 +12,21 @@ def CountFrequency(my_list):
     for items in my_list:
         freq[items] = my_list.count(items)
     return freq
+
+def fastMode(dataset):
+    # assume all values in dataset
+    # are between 0 and 99 inclusive
+
+    # 1. make a list of 100 slots
+    # and set them all to 0
+    # this will store our tallies
+
+    # 2. Loop through our dataset
+    # and for each item incremement
+    # (add 1) to the appropriate
+    # slot in the tallies list
+
+    # 3. the index with the highest
+    # value in tallies is the mode
+
+    pass
