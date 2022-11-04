6f8a96e26f615e133759c6f66cd2123d9a9a37ff
Irene06he <104945913+Irene06he@users.noreply.github.com>
2022-11-04 10:25:50 +0000

    I modified file madlibs.py

--- project-01-madlibs/madlibs.py
+++ project-01-madlibs/madlibs.py
@@ -1,6 +1,6 @@
 import random
 
-f = open('story.py')# extras 1, and I received assistance from Kai Min
+f = open('story.py')# extras 1 --> write a story in a file and read it from your program, and I received assistance from Kai Min
 story = f.read()
 f.close()
 
@@ -14,6 +14,6 @@ def gets_newstory (story)  :
     for word in story.split():
         new_story = new_story +[word.replace('<verb>',random.choice(verb)).replace('<noun>',random.choice(noun))]
         result = " ".join(new_story)
-    return result.replace('<hero>',random.choice(hero)) # extras 2
+    return result.replace('<hero>',random.choice(hero)) # extras 2-->In this case, each <VERB> and <NOUN> would be replaced by a random selection from the word lists but <HERO> would randomly choose a hero once and then use that name for all instances of <HERO>
 
 print(gets_newstory (story))
