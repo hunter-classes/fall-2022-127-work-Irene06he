6c5b3ae1636fa3395d8c08e1b0af55988b0a8279
Irene06he <104945913+Irene06he@users.noreply.github.com>
2022-11-04 10:47:14 +0000

    I modified file story.py

--- project-01-madlibs/story.py
+++ project-01-madlibs/story.py
@@ -1,4 +1,4 @@
 First, <hero> always <verb> in the <noun>.
 Then, <hero> usually <verb> in the <noun>.
 Later, <hero> <verb> around the <noun>.
-Finally,<hero> <verb> in the <noun>.
+Finally, <hero> <verb> in the <noun>.
