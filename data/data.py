a4dad1e5653236037a9f1d2dfb5e5ebb6438e6ce
Irene06he <104945913+Irene06he@users.noreply.github.com>
2022-12-14 11:17:01 +0000

    I add folder data, file data.py, file wind_turbines.csv, and file hydropower.csv

---
+++ data/data.py
@@ -0,0 +1,45 @@
+# I finished one extra : Use more than one data source and have my analysis compare,contrast, or correlate them.
+
+# Basic 
+import csv
+
+reader = csv.reader(open("wind_turbines.csv"))
+data=[]
+for row in reader:
+    data.append(row)
+    print(row)
+  
+# Look for average
+height = [float(row[7]) for row in data[1:]]
+def get_Height(height):
+  sum_height = sum(height)
+  count_height = len(height)
+  average = sum_height / count_height
+  return average
+print("The average of total height of the turbine:", get_Height(height))
+
+# Second data source
+# Basic 
+import csv
+
+f = csv.reader(open("hydropower.csv"))
+number=[]
+for row in f:
+    number.append(row)
+    print(row)
+  
+# Look for average
+Structural_Height = [float(row[2]) for row in number[1:]]
+def get_Height(Structural_Height):
+  sum_height = sum(Structural_Height)
+  count_height = len(Structural_Height)
+  average = sum_height / count_height
+  return average
+print("The average of Structural Height:", get_Height(Structural_Height))
+
+# Compare
+if get_Height(height) < get_Height(Structural_Height):
+  print("The average of Structural Height is greater than the average of total height of the turbine")
+else:
+  print("The average of Structural Height is less than the average of total height of the turbine")
+
